d3141c015a15f95b88f78927f2a3951fb6d280ba
Stefan Kapferer <stefan@kapferer.ch>
2016-05-13 07:19:36 +0200

    Applied Muriel's changes

--- solution/scripts/font-size.js
+++ solution/scripts/font-size.js
@@ -1,1 +1,12 @@
+sliderClickHandler = function() {
+    var tag = document.getElementsByTagName("body");
+    tag.fontSize(changeFont);
+}
+
+("#font-slider").onclick = sliderClickHandler;
+
+function changeFont () {
+    var font = 10;
+    return font;
+}
 
