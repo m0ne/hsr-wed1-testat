b991c62ead9bf10521b341ba71d4346711c5780d
Stefan Kapferer <stefan@kapferer.ch>
2016-05-19 20:25:23 +0200

    Remove unused script

--- solution/scripts/font-size.js
+++ solution/scripts/font-size.js
@@ -1,12 +1,1 @@
-sliderClickHandler = function() {
-    var tag = document.getElementsByTagName("body");
-    //tag.fontSize(changeFont); <-- function does not exist
-}
-
-//("#font-slider").onclick = sliderClickHandler;
-
-function changeFont () {
-    var font = 10;
-    return font;
-}
 
